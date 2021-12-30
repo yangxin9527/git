846fdccba2f36679d583d1d90dacea3915a74784
yangxin <yx5316@dingtalk.com>
2021-12-30 14:02:50 +0800

    merge

--- merge.mjs
+++ merge.mjs
@@ -4,8 +4,9 @@ import fs from "fs";
 import path from "path";
 const log = console.log;
 const gitBaseUrl = "../we-work-webapp-new";
-const baseBranch = "test-v21.52";
-const targetBranch = "yx/feature-wb-6271-service-merge";
+const baseBranch = "test-v21.53.1";
+const targetBranch = "yx/feature-wb-8458-project-add-file-type";
+// const targetBranch = "yx/feature-wb-6271-service-merge";
 async function main() {
   log(chalk.green("开始"));
   try {
@@ -15,40 +16,38 @@ async function main() {
     await git.pull("origin", baseBranch);
     let mergeSuccess = true;
     try {
-      await git.merge([targetBranch], (e) => {
-        if (e && e.git) {
-          //如果是资源文件冲突，解决冲突 assets-url-map assets-rev
-          log("冲突文件：", e.git.merges);
-          // 冲突只有资源文件
-          if (
-            new Set([...[e.git.merges], ...["assets-url-map", "assets-rev"]])
-              .size === 2
-          ) {
-            ["assets-url-map", "assets-rev"].map((file) => {
-              if (file === "assets-rev") {
-                const txtPath = path.resolve(`${gitBaseUrl}/${file}`);
-                fs.readFile(txtPath, "utf8", function (err, data) {
-                  if (err) {
-                    return console.log(err);
-                  }
-                  let result = data.replace(/<<<<<<<.*[\r\n]/g, "");
-                  result = result.replace(/=======.*[\r\n]/g, "");
-                  result = result.replace(/>>>>>>>.*[\r\n]/g, "");
-                  fs.writeFile(txtPath, result, "utf8", function (err) {
-                    if (err) return console.log(err);
-                  });
-                });
-              }
-            });
-            git
-              .add(e.git.merges)
-              .commit(`Merge branch ${targetBranch} into ${baseBranch}`);
-          } else {
-            mergeSuccess = false;
-          }
-        }
-      });
+      await git.pull("origin", targetBranch);
     } catch (e) {}
+    const statusResult = await git.status()
+    if (statusResult && statusResult.conflicted && statusResult.conflicted.length !== 0) {
+     
+      log("冲突文件：", statusResult.conflicted);
+     //如果是资源文件冲突，解决冲突 assets-url-map assets-rev
+      if (
+        new Set([...statusResult.conflicted, ...["assets-url-map", "assets-rev"]])
+          .size === 2
+      ) {
+        ["assets-url-map", "assets-rev"].map((file) => {
+            const txtPath = path.resolve(`${gitBaseUrl}/${file}`);
+            fs.readFile(txtPath, "utf8", function (err, data) {
+              if (err) {
+                return console.log(err);
+              }
+              let result = data.replace(/<<<<<<<.*[\r\n]/g, "");
+              result = result.replace(/=======.*[\r\n]/g, "");
+              result = result.replace(/>>>>>>>.*[\r\n]/g, "");
+              fs.writeFile(txtPath, result, "utf8", function (err) {
+                if (err) return console.log(err);
+              });
+            });
+        });
+        git
+          .add(statusResult.conflicted)
+          .commit(`Merge branch ${targetBranch} into ${baseBranch}`);
+      } else {
+        mergeSuccess = false;
+      }
+    }
     if (mergeSuccess) {
       await git.push("origin", baseBranch);
       log(chalk.green("成功"));
@@ -63,6 +62,4 @@ async function main() {
 }
 main();
 
-// 如果资源文件冲突，自动处理
-// const statusResult = await git.status()
-// log(statusResult.conflicted)
+
